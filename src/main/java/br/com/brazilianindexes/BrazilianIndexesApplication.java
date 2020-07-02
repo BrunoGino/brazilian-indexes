f2b275b0192ee87a944b7f8299df805f650c54e8
Bruno Gino <bruno.gino24@gmail.com>
2020-07-02 15:27:55 -0300

    Created SpringBoot project

---
+++ src/main/java/br/com/brazilianindexes/BrazilianIndexesApplication.java
@@ -0,0 +1,13 @@
+package br.com.brazilianindexes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrazilianIndexesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrazilianIndexesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/brazilianindexes/BrazilianIndexesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brazilianindexes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrazilianIndexesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
